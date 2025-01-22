387eaa70f3a9d047684782bba5f9f01fae9ca70f
Ben Wernicke <ben@wernicke.at>
2025-01-22 11:26:56 +0100

    stuff

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+    description = "System Configuration";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    };
+
+    outputs = { self, nixpkgs, ... }: {
+        nixosConfigurations.bigbox = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                ./configuration.nix
+            ];
+        };
+    };
+}
