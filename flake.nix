15ff140c5235aecc73d2e3c11eece007ca1dbf1b
Ben Wernicke <ben@wernicke.at>
2025-01-24 09:51:11 +0100

    unstable

--- flake.nix
+++ flake.nix
@@ -1,18 +1,33 @@
 {
-    description = "Femboy Config";
+    description = "bendermeister system configuration";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { nixpkgs, ... }: {
+    outputs = { nixpkgs, home-manager, ... }: 
+    let
+        system = "x86_64-linux";
+    in
+    {
         nixosConfigurations = {
             bigbox = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
+                inherit system;
                 modules = [
                     ./modules/bigbox/configuration.nix
                     ./modules/bigbox/hardware-configuration.nix
                     ./modules/common/common.nix
+
+
+                    home-manager.nixosModules.home-manager {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.users.ben = import ./home/home.nix;
+                    }
                 ];
             };
         };
